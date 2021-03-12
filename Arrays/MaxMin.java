f98f7c46fa6ed483fa73ec07958fb5aa5f53b05c
tejassg <56124339+tejas-gudulekar@users.noreply.github.com>
2021-03-12 18:49:59 +0530

    Create MaxMin.java

---
+++ Arrays/MaxMin.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+public class MaxMin{
+	
+  //Method to get maximum element using naive method
+	static int getMax(int arr[]) {
+		int max = arr[0];
+		int i = 0;
+		
+		while(i<arr.length) {
+			if(arr[i]>max) {
+				max = arr[i];
+			}
+			i++;
+		}
+		return max;
+	}
+  
+  //Function to get minimum element using naive method
+	static int getMin(int[] arr) {
+		int min = arr[0], i=0;
+		while(i<arr.length) {
+			if(arr[i]<min) {
+				min = arr[i];
+			}
+			i++;
+		}
+		return min;
+	}
+  
+  
+	public static void main(String[] args) {
+		int[] array  = {7,2,5,72,5,102,84,35,200,1};
+    
+    //Method 1: Using array sort method
+    Arrays.sort(array);
+		System.out.println("Maximum Element " + array[array.length - 1]);
+		System.out.println("Minimum Element " + array[0]);
+		
+    //Method 2: Using naive method; Functions are wriiten above
+    /* 
+    System.out.println("Maximum Element " + getMax(array));
+		System.out.print("Minimum Element " + getMin(array));  
+    */
+		
+		
+		
+	}
+}
