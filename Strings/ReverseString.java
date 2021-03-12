857c5d513b2cf551507de3c58b3c56b04a10b8a3
tejassg <56124339+tejas-gudulekar@users.noreply.github.com>
2021-03-12 15:16:15 +0530

    Update ReverseString.java

--- Strings/ReverseString.java
+++ Strings/ReverseString.java
@@ -1,22 +1,23 @@
-package basicProject;
 import java.util.Scanner;
 
 
-public class helloWorld{
+public class ReverseString{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
 		sc.close();
 		
+		//Note: It is always good to check for input like whether the String is 1 or no character along or whether the input is String or not
+		
 		/* Converting String to Array uusing naive Approach		
-		 char[] charArray = new char[str.length()];
+		char[] charArray = new char[str.length()];
 		
 		for(int i =0 ; i<str.length(); i++) {
 			charArray[i] = str.charAt(i);
 		} */
 		
 		
-    //Converting using toCharArrayMethod
+    		//Converting using toCharArrayMethod
 		char[] charArray = str.toCharArray();
 		
 		//Printing the Array Elements
