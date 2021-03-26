8357fa9c373c446a7416a7550de7dceb90684eeb
tejassg <56124339+tejas-gudulekar@users.noreply.github.com>
2021-03-26 11:09:46 +0530

    Create Tables.java

---
+++ Numericals/Tables.java
@@ -0,0 +1,12 @@
+public class Tables{
+	
+	public static void main(String[] args) {
+		int n=2;
+		for(int i=1;i<=10;i++) {
+			for(int j=1;j<=20;j++) {
+				System.out.print(i*j + " ");
+			}
+			System.out.println();
+		}
+	}
+}
