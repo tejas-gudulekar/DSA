bd51f4b4e5c08b38a2ede1fbb3bb6492ae683b29
tejassg <56124339+tejas-gudulekar@users.noreply.github.com>
2021-04-05 16:05:26 +0530

    Update CountSmaller.java

--- Arrays/CountSmaller.java
+++ Arrays/CountSmaller.java
@@ -1,18 +1,29 @@
+/*
+ 	Given an Sorted Array, n (size of Array) and x value. We need to return the count of all the elements
+ 	which are smaller or equal to x;
+ 	Input: {1, 2, 4, 5, 8, 10}, n = 6, x = 9;
+ 	Output: 5
+ */
 
+import java.util.Arrays;
 
-public class CountSmaller{
-	static void countSmaller(int[] arr,int n) {
-		int count = 0;
-		for(int i =0;i<arr.length;i++) {
-			if(arr[i] <= n) {
-				count++;
-			}
-		}
-		System.out.print(count);
-	}
-	public static void main(String[] args) {
-		int arr[] = {1, 2, 2, 2, 5, 7, 9};
-		int lessThen = 2;
-		countSmaller(arr,lessThen);
+public class Array{
+	  static public int countOfElements(int arr[], int n, int x)
+	    {
+	        int count=0;
+	        int i = 0;
+	        //Gotcha: If we check the arr[i] condition first, then in some cases it can give out of bound error
+	        while(i<n && arr[i]<=x){
+	            i++;
+	            count++;
+	        }
+	        return count;
+	    }
+	
+ public static void main(String[] args) {
+	 int[] arr = {2, 5, 1, 99, 45, 23, 36, 0};
+	Arrays.sort(arr);
+	int count = countOfElements(arr,arr.length,99);
+	 System.out.println(count);
 	}
 }
