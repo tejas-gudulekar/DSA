3e783106cc1e5992444bf05451a7eb7281841c7e
tejassg <56124339+tejas-gudulekar@users.noreply.github.com>
2021-04-05 19:20:42 +0530

    Update AlternateElements.java

--- Arrays/AlternateElements.java
+++ Arrays/AlternateElements.java
@@ -5,7 +5,7 @@
  	Output: 1 3 5 7
  */
 
-public class Arrays{
+public class AlternateElements{
 	static void alternate(int[] arr) {
 		for(int i=0;i<arr.length;i+=2) {
 			System.out.print(arr[i] + " ");
