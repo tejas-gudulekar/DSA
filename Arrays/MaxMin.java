a40516d8d70355adea3e56e3ce10ab53014a9378
tejassg <56124339+tejas-gudulekar@users.noreply.github.com>
2021-04-05 14:40:34 +0530

    Update MaxMin.java

--- Arrays/MaxMin.java
+++ Arrays/MaxMin.java
@@ -1,53 +1,29 @@
-import java.util.Arrays;
 
-/* 
-Problem: To find the maximum and minimum element in array
-Input: 2 3 35 76 102 1
-Output Max: 102 and Min: 1
-*/
-
-public class MaxMin{
-	
-  //Method to get maximum element using naive method
-	static int getMax(int arr[]) {
+/*
+ 	Given an unsorted array we need to return the Max and Min Element from the array;
+ 	TimeComplexity should be of O(n);
+ 	Input: {2, 5, 1, 99, 45, 23, 36, 0};
+ 	Output: 0 99
+ */
+public class Arrays{
+	static void MinMax(int[] arr){
+		int min = arr[0];
 		int max = arr[0];
-		int i = 0;
-		
-		while(i<arr.length) {
-			if(arr[i]>max) {
-				max = arr[i];
-			}
-			i++;
-		}
-		return max;
-	}
-  
-  //Function to get minimum element using naive method
-	static int getMin(int[] arr) {
-		int min = arr[0], i=0;
-		while(i<arr.length) {
+		for(int i=0;i<arr.length;i++) {
+			
 			if(arr[i]<min) {
 				min = arr[i];
 			}
-			i++;
+			
+			if(arr[i] > max) {
+				max = arr[i];
+			}
 		}
-		return min;
+		
+		System.out.println("Min: " + min + " Max: " + max);
 	}
-  
-  
-	public static void main(String[] args) {
-		int[] array  = {7,2,5,72,5,102,84,35,200,1};
-    
-    //Method 1: Using array sort method
-    Arrays.sort(array);
-		System.out.println("Maximum Element " + array[array.length - 1]);
-		System.out.println("Minimum Element " + array[0]);
-		
-    //Method 2: Using naive method; Functions are wriiten above
-    /* 
-    System.out.println("Maximum Element " + getMax(array));
-		System.out.print("Minimum Element " + getMin(array));  
-    */
-		
-   }
+ public static void main(String[] args) {
+	 int[] arr = {2, 5, 1, 99, 45, 23, 36, 0};
+	 MinMax(arr);
+ 	}
 }
