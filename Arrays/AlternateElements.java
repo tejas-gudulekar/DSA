f7089739f329ee4f0b3914b3094afeb89a7c2ac5
tejassg <56124339+tejas-gudulekar@users.noreply.github.com>
2021-04-05 14:24:43 +0530

    Update AlternateElements.java

--- Arrays/AlternateElements.java
+++ Arrays/AlternateElements.java
@@ -1,30 +1,18 @@
-import java.util.Scanner;
 
-//Probelm: To print alternate elements
-//Input: 5
-//Array Elements: 1 2 3 4 5 6
-//Output: 1 3 5
+/*
+ 	Given a array we need to print alternate numbers, starting from index 0;
+ 	Input: { 1, 2, 3, 4, 5, 6, 7}
+ 	Output: 1 3 5 7
+ */
 
-
-public class AlternateElements{
+public class Arrays{
+	static void alternate(int[] arr) {
+		for(int i=0;i<arr.length;i+=2) {
+			System.out.print(arr[i] + " ");
+		}
+	}
 	public static void main(String[] args) {
-		
-		System.out.print("Enter the array Size");
-		Scanner sc = new Scanner(System.in);
-    
-		int n = sc.nextInt();
-		int[] array = new int[n];
-		
-		System.out.print("Enter array elements");
-		for(int i=0;i<n;i++) {
-			array[i] = sc.nextInt();
-		}
-		
-		System.out.print("Alternate elements are: ");
-		int i = 0;
-		while(i<n) {
-			System.out.print(array[i]+ " ");
-			i += 2;
-		}
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7};
+		alternate(arr);
 	}
 }
