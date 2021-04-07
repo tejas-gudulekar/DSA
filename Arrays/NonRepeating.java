40a8a95bf6b56f1e0afcce724f34c11433404384
tejassg <56124339+tejas-gudulekar@users.noreply.github.com>
2021-04-07 15:28:46 +0530

    Create NonRepeating.java

---
+++ Arrays/NonRepeating.java
@@ -0,0 +1,32 @@
+/*
+ * Given an Array,we need to return the first element is repeating only once
+ */
+import java.util.*;
+public class NonRepeating{
+	static int nonRepeating(int[] arr) {
+		HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+		for(int i=0;i<arr.length;i++) {
+			if(!map.containsKey(arr[i])) {
+				map.put(arr[i], 1);
+			}
+			else {
+				int frequncy = map.get(arr[i]);
+				frequncy++;
+				map.put(arr[i],frequncy);
+			}
+		}
+		
+		for(int i=0;i<arr.length;i++) {
+			if(map.get(arr[i])<2) {
+				return arr[i];
+			}
+		}
+		
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {-1, 2, -1, 3, 2};
+		System.out.println(nonRepeating(arr));
+ }
+}
