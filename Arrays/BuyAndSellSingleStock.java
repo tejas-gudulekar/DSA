eda34a4019af5c912111426559e1aece7ccfdfdc
tejassg <56124339+tejas-gudulekar@users.noreply.github.com>
2021-04-11 16:10:45 +0530

    Update BuyAndSellSingleStock.java

--- Arrays/BuyAndSellSingleStock.java
+++ Arrays/BuyAndSellSingleStock.java
@@ -7,31 +7,26 @@ public class BuyAndSellSingleStock{
 		int profit = 0;
 		int sell_on = 0;
 		int buy_on = 0;
+		int min_index = 0;
 		
 		for(int i=0;i<n;i++) {
 			if(arr[i] < min_buy_price) {
 				min_buy_price = arr[i];
+				min_index = i;
 			}
 			
 			if(arr[i]- min_buy_price > profit) {
 				profit = (arr[i]- min_buy_price);
 				sell_on = i;
-			}
-		}
-		
-		//To get the buy on
-		for(int i=0;i<sell_on;i++) {
-			if(arr[sell_on] - arr[i] == profit) {
-				buy_on = i;
-				break;
+				buy_on = min_index;
 			}
 		}
 		
 		System.out.println("Buy on: " + buy_on + " Sell on: " + sell_on + " for maximum profit of " + profit);
 	}
 	public static void main(String[] args) {
-		int[] arr = {3,5,2,198,1,7,4,9,5,27};
+		int[] arr = {0,5,2,198,1,7,4,9,5};
 		getMaxProfit(arr,arr.length);
 		
 	}
-}
+}}
