167f1052e6f38e05597f66cb2b98234e4fa56b5c
tejassg <56124339+tejas-gudulekar@users.noreply.github.com>
2021-04-27 19:26:30 +0530

    Update Queue.java

--- Queue/Queue.java
+++ Queue/Queue.java
@@ -1,86 +1,66 @@
-
-
+/*
+Implementing Queue using LinkedList
+*/
+class node{
+	node next;
+	int data;
+	node(int data){
+		this.data = data;
+	}
+}
 
 public class Queue{
 	node front;
 	node rear;
-	int lenght;
-	
-	class node{
-		node next;
-		int data;
-		
-		node(int data){
-			this.data = data;
-		}
-	}
 	
 	void enqueue(int data) {
 		node newNode = new node(data);
 		
-		if(lenght == 0) {
+		if(front==null) {
 			front = newNode;
 			rear = newNode;
-			lenght++;
 			return;
 		}
 		
 		rear.next = newNode;
 		rear = newNode;
-		lenght++;
 	}
 	
 	void dequeue() {
-		if(lenght==0) {
+		if(front==null) {
 			System.out.println("Queue is empty");
-			return;
 		}
-		else if(lenght==1) {
-			System.out.println("Dequeue Element: " + front.data);
-			front=null;
-			rear=null;
-			lenght--;
-			return;
+		else if(front==rear) {
+			front = null;
+			rear = null;
 		}
-		System.out.println("Dequeue Element: " + front.data);
-		front = front.next;
-		lenght--;
-	}
-	
-	void peek() {
-		if(lenght==0) {
-			System.out.println("Queue is empty");
-			return;
+		else {
+			front = front.next;
 		}
-		System.out.println("Peek Element: " + front.data);
 	}
 	
 	void print() {
-		if(lenght == 0) {
+		if(front==null) {
 			System.out.println("Queue is empty");
 			return;
 		}
+		
 		node tempNode = front;
 		while(tempNode!=null) {
 			System.out.print(tempNode.data + " ");
 			tempNode = tempNode.next;
 		}
-		
-		System.out.println();
 	}
-	
 	public static void main(String[] args) {
-		Queue queue = new Queue();
-		queue.enqueue(5);
-		queue.enqueue(7);
-		queue.enqueue(9);
-		queue.peek();		
-		queue.dequeue();
-		queue.dequeue();
-		queue.dequeue();
-		queue.dequeue();
-		System.out.print(queue.lenght);
-		queue.print();
-		System.out.println(queue.front + " " + queue.rear);
+		Queue q = new Queue();
+		q.enqueue(1);
+		q.enqueue(2);
+		q.enqueue(3);
+		q.enqueue(4);
+		q.dequeue();
+		q.dequeue();
+		q.dequeue();
+		q.dequeue();
+		q.print();
 	}
 }
