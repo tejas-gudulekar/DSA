acb3a16333d90c02cf2762dd10331d5a195f78e7
tejassg <56124339+tejas-gudulekar@users.noreply.github.com>
2021-05-28 13:25:24 +0530

    Update MergeSort.java

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -1,67 +1,66 @@
-
-
-
-
-public class MergeSort {
-    void mergeSort(int[] array) {
-        int[] helperArray = new int[array.length];
-        mergeSort(array, helperArray, 0, array.length - 1);
-    }
-
-    void mergeSort(int[] array, int[] helperArray, int leftStart, int rightEnd) {
-        if (leftStart < rightEnd) {
-            int middle = (leftStart + rightEnd) / 2;
-            System.out.println("Left Divide: " +  leftStart + " "+ middle);
-            mergeSort(array, helperArray, leftStart, middle);
-            System.out.println("Right Divide: " +  Integer.toString(middle+1) + " " +  rightEnd);
-            mergeSort(array, helperArray, middle + 1, rightEnd);
-            System.out.println("Merge halves: " + leftStart +" "+ middle +" " + rightEnd);
-            merge(array, helperArray, leftStart, middle, rightEnd);
-
-        }
-     }
-
-    void merge(int[] array, int[] helperArray, int leftStart, int middle, int rightEnd) {
-        // TODO Auto-generated method stub
-        for (int i = leftStart; i <= rightEnd; i++) {
-            helperArray[i] = array[i];
-        }
-
-        int helperLeft = leftStart;
-        int helperRight = middle + 1;
-        int current = leftStart;
-
-        while (helperLeft <= middle && helperRight <= rightEnd) {
-            if (helperArray[helperLeft] <= helperArray[helperRight]) {
-                array[current] = helperArray[helperLeft];
-                helperLeft++;
-            } else {
-                array[current] = helperArray[helperRight];
-                helperRight++;
-            }
-            current++;
-
-        }
-        int reamining = middle - helperLeft;
-        for (int i = 0; i <= reamining; i++) {
-            array[current + i] = helperArray[helperLeft + i];
-        }
-    }
-
-    public static void main(String[] args) {
-        MergeSort m = new MergeSort();
-        int[] arr = {
-            92,
-            1,
-            54,
-            33,
-            7,
-            2,
-            96
-        };
-        m.mergeSort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
+public class MergeSort{
+	
+	void mergeSort(int[] arr) {
+		int[] temp = new int[arr.length];
+		mergeSort(arr,temp,0,arr.length-1);
+	}
+	
+	void mergeSort(int[] arr,int[] temp, int left, int right) {
+		if(left<right) {
+			int mid = (left+right)/2;
+			mergeSort(arr,temp,left,mid);
+			mergeSort(arr,temp,mid+1,right);
+			mergeHalves(arr,temp,left,mid,right);
+		}
+	}
+	
+	void mergeHalves(int[] arr,int[] temp,int left,int mid, int right) {
+		
+		int i = left;
+		int j = mid+1;
+		int k = left;
+		
+		
+		while(i<=mid && j<=right) {
+			if(arr[i]<=arr[j]) {
+				temp[k] = arr[i];
+				i++;
+			}
+			else {
+				temp[k] = arr[j];
+				j++;
+			}
+			k++;
+		}
+		
+		if(i>mid) {
+			while(j<=right) {
+				temp[k] = arr[j];
+				k++;
+				j++;
+			}
+		}
+		else {
+			while(i<=mid) {
+				temp[k] = arr[i];
+				k++;
+				i++;
+			}
+		}
+		
+		
+		for(int p=left;p<=right;p++) {
+			arr[p] = temp[p];
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {9, 5, 11, 2, 17, 4, 23, 18, 7, 1};
+		MergeSort m = new MergeSort();
+		m.mergeSort(arr);
+		
+		for(int i = 0;i<arr.length;i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
 }
